0dc096c7dc9c11f8199149b39b51aae96575736f
Kaz Wolfe <root@kazwolfe.io>
2022-05-06 20:36:05 -0700

    Version 0.1.12 Bump
    - Add some debug things
    - Version bumping shenanigans

--- FFXIVPlugin/UI/Windows/DebugWindow.cs
+++ FFXIVPlugin/UI/Windows/DebugWindow.cs
@@ -1,11 +1,16 @@
 ﻿using System.Numerics;
+using System.Reflection;
+using Dalamud.Interface.Colors;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
 using XIVDeck.FFXIVPlugin.Server;
+using XIVDeck.FFXIVPlugin.Server.Helpers;
 using XIVDeck.FFXIVPlugin.Server.Messages.Outbound;
+using XIVDeck.FFXIVPlugin.Utils;
 
 namespace XIVDeck.FFXIVPlugin.UI.Windows; 
 
+// ReSharper disable once UnusedType.Global - Used in #DEBUG builds from SettingsWindow
 public class DebugWindow : Window  {
     private const string WindowKey = "XIVDeck Debug Tools";
     
@@ -34,7 +39,7 @@ public class DebugWindow : Window  {
     }
     
     public override void Draw() {
-        ImGui.Text("Hello, world!");
+        ImGui.Text("Hello, world! Enjoying your time behind the curtain?");
 
         if (ImGui.Button("Clear Icons")) {
             XIVDeckWSServer.Instance?.BroadcastMessage(new WSStateUpdateMessage("DEBUG_ClearIcons"));
@@ -43,6 +48,28 @@ public class DebugWindow : Window  {
         ImGui.SameLine();
         if (ImGui.Button("Redraw All Icons")) {
             XIVDeckWSServer.Instance?.BroadcastMessage(new WSStateUpdateMessage("IconCache"));
+        }
+
+        if (ImGui.Button("Resend Init Command")) {
+            var xivPluginVersion = Assembly.GetExecutingAssembly().GetName().Version!;
+            var packet = new WSInitReplyMessage(xivPluginVersion.GetMajMinBuild(), AuthHelper.Instance.Secret);
+            XIVDeckWSServer.Instance?.BroadcastMessage(packet);
+        }
+        
+        ImGui.Spacing();
+        
+        ImGui.PushStyleColor(ImGuiCol.Text, ImGuiColors.DalamudRed);
+        if (ImGui.Button("KILL SERVER")) {
+            var field = XIVDeckPlugin.Instance.GetType().GetField("_xivDeckWebServer",
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var ws = (XIVDeckWebServer) field?.GetValue(XIVDeckPlugin.Instance)!;
+            ws.Dispose();
+        }
+        ImGui.PopStyleColor();
+        ImGui.SameLine();
+        if (ImGui.Button("(Re)Start Server")) {
+            XIVDeckPlugin.Instance.InitializeWebServer();
         }
     }
 }
