e8f230d273cede7b0a9e89a7de4956f8d8bd819f
Kaz Wolfe <root@kazwolfe.io>
2022-07-01 18:32:27 -0700

    Switch Penumbra IPC to be opportunistic
    The Penumbra IPC will behave in a more opportunistic manner, which generally should be more stable as versions change (and also be more resilient to load-time shenanigans).

--- FFXIVPlugin/IPC/Subscribers/PenumbraIPC.cs
+++ FFXIVPlugin/IPC/Subscribers/PenumbraIPC.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Logging;
 using Dalamud.Plugin.Ipc;
+using Dalamud.Plugin.Ipc.Exceptions;
 using XIVDeck.FFXIVPlugin.Base;
-using XIVDeck.FFXIVPlugin.Game;
-using XIVDeck.FFXIVPlugin.Resources.Localization;
 
 namespace XIVDeck.FFXIVPlugin.IPC.Subscribers;
 
 [PluginIpc]
 internal class PenumbraIPC : IPluginIpcClient {
-    private const int PenumbraIPCCompatVersion = 5;
-    
     // note: this is *extremely fragile* and honestly bad, but this will be instantiated by the system at a higher
     // level. if we want to consume the IPC, this should be a safe-ish way to do it, assuming null checks are used.
     internal static PenumbraIPC? Instance;
@@ -21,7 +17,7 @@ internal class PenumbraIPC : IPluginIpcClient {
     private ICallGateSubscriber<string, string>? _penumbraResolveDefaultSubscriber;
 
     private readonly ICallGateSubscriber<object?> _penumbraRegisteredSubscriber;
-
+    
     internal PenumbraIPC() {
         Instance = this;
 
@@ -42,10 +38,12 @@ internal class PenumbraIPC : IPluginIpcClient {
         this._penumbraApiVersionSubscriber = null;
         this._penumbraResolveDefaultSubscriber = null;
 
+        this.Enabled = false;
+
         GC.SuppressFinalize(this);
     }
 
-    public int Version => this._penumbraApiVersionSubscriber?.InvokeFunc() ?? 0;
+    public int Version => this._penumbraApiVersionSubscriber?.InvokeFunc() ?? -1;
 
    private void _initializeIpc() {
        if (!Injections.PluginInterface.PluginNames.Contains("Penumbra")) {
@@ -53,28 +51,25 @@ internal class PenumbraIPC : IPluginIpcClient {
            return;
        }
        
-       var versionEndpoint = Injections.PluginInterface.GetIpcSubscriber<int>("Penumbra.ApiVersion");
+       this._penumbraApiVersionSubscriber = Injections.PluginInterface.GetIpcSubscriber<int>("Penumbra.ApiVersion");
+       this._penumbraResolveDefaultSubscriber = Injections.PluginInterface.GetIpcSubscriber<string, string>("Penumbra.ResolveDefaultPath");
 
-       // this line may explode with an exception, but that should be fine as we'd normally catch that.
-       var version = versionEndpoint.InvokeFunc();
-       
-       this._penumbraApiVersionSubscriber = versionEndpoint;
-
-       if (version == PenumbraIPCCompatVersion) {
-           this._penumbraResolveDefaultSubscriber = Injections.PluginInterface.GetIpcSubscriber<string, string>("Penumbra.ResolveDefaultPath");
-           
-           this.Enabled = true;
-           PluginLog.Information("Enabled Penumbra IPC connection!");
-       } else {
-           PluginLog.Warning($"Penumbra IPC detected, but version {version} is incompatible!");
-           DeferredChat.SendOrDeferMessage(new SeStringBuilder()
-               .AddUiForeground($"[{UIStrings.XIVDeck}] ", 514)
-               .AddText(UIStrings.PenumbraIPC_VersionMismatch)
-               .Build());
-       }
+       this.Enabled = true;
    }
    
     public string ResolvePenumbraPath(string path) {
-        return this._penumbraResolveDefaultSubscriber?.InvokeFunc(path) ?? path;
+        if (!this.Enabled || this._penumbraResolveDefaultSubscriber == null) return path;
+
+        try {
+            return this._penumbraResolveDefaultSubscriber.InvokeFunc(path);
+        } catch (IpcNotReadyError) {
+            PluginLog.Debug("Got a NotReadyError trying to call ResolveDefaultPath. Falling back to normal path");
+            return path;
+        } catch (Exception ex) {
+            PluginLog.Error(ex, "Failed to invoke Penumbra IPC, disabling!");
+            this.Enabled = false;
+            
+            return path;
+        }
     }
 }
