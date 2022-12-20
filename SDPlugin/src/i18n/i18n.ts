3c22a8e3a8b660530829402d2aaadb4c328ba009
Kaz Wolfe <root@kazwolfe.io>
2022-12-19 22:43:22 -0800

    Switch package manager to yarn
    - Switch the actual package manager accordingly
    - Ignore streamdeck-ts as well for now
    - Simplify i18next loading

--- SDPlugin/src/i18n/i18n.ts
+++ SDPlugin/src/i18n/i18n.ts
@@ -1,12 +1,9 @@
 ﻿import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import * as resources from '../../assets/locales'
+import * as resources from '../../assets/locales';
 
-i18n
-    .use(initReactI18next)
-    .init({
-        resources: resources,
-        fallbackLng: "en",
-    });
+i18n.init({
+    resources: resources,
+    fallbackLng: "en"
+});
 
 export default i18n;
--- SDPlugin/src/i18n/react-i18next.d.ts
+++
@@ -1,9 +0,0 @@
-﻿import 'react-i18next';
-import * as resources from '../../assets/locales'
-
-declare module 'react-i18next' {
-    interface CustomTypeOptions {
-        defaultNS: 'common';
-        resources: resources
-    }
-}
