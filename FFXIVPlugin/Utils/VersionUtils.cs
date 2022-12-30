55348f2cc163500bfe510095b19f48479520f48f
Kaz Wolfe <root@kazwolfe.io>
2022-12-29 20:58:15 -0800

    Fix a bug with improper "olderThan detection"
    - A logic bug caused non-builds to be ignored

--- FFXIVPlugin/Utils/VersionUtils.cs
+++ FFXIVPlugin/Utils/VersionUtils.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
-namespace XIVDeck.FFXIVPlugin.Utils; 
+namespace XIVDeck.FFXIVPlugin.Utils;
 
 public static class VersionUtils {
     public static string GetCurrentMajMinBuild() {
         return Assembly.GetExecutingAssembly().GetName().Version!.GetMajMinBuild();
     }
-    
+
     public static string GetMajMinBuild(this Version version) {
         return $"{version.Major}.{version.Minor}.{version.Build}";
     }
@@ -19,12 +20,12 @@ public static class VersionUtils {
     /// <param name="right">The version to compare relative to.</param>
     /// <returns>Returns true if the passed version is newer than the base version.</returns>
     public static bool IsOlderThan(this Version left, Version right) {
-        if (left.Major < right.Major) 
-            return true;
-        
-        if (left.Minor < right.Minor) 
-            return true;
-        
+        if (left.Major < right.Major) return true;
+        if (left.Major > right.Major) return false;
+
+        if (left.Minor < right.Minor) return true;
+        if (left.Minor > right.Minor) return false;
+
         return left.Build < right.Build;
     }
 }
