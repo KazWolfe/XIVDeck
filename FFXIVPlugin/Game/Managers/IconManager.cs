14d5f6646cf5b79fdeb212663a6d95182a3946a2
Kaz Wolfe <root@kazwolfe.io>
2023-01-13 17:10:27 -0800

    Add skeleton ULD endpoint

--- FFXIVPlugin/Game/Managers/IconManager.cs
+++ FFXIVPlugin/Game/Managers/IconManager.cs
@@ -1,33 +1,27 @@
 ﻿using System;
 using Dalamud.Logging;
 using Lumina.Data.Files;
+using Lumina.Data.Parsing.Uld;
 using XIVDeck.FFXIVPlugin.Base;
 using XIVDeck.FFXIVPlugin.IPC.Subscribers;
 using XIVDeck.FFXIVPlugin.Utils;
 
-namespace XIVDeck.FFXIVPlugin.Game.Managers; 
+namespace XIVDeck.FFXIVPlugin.Game.Managers;
 
 // borrowed from https://github.com/Caraxi/RemindMe/blob/master/IconManager.cs
 public static class IconManager {
     private const string IconFileFormat = "ui/icon/{0:D3}000/{1}{2:D6}{3}.tex";
 
-    private static string GetIconPath(string lang, int iconId, bool hq = false, bool highres = false, bool forceOriginal = false) {
-        var path = string.Format(IconFileFormat, 
-            iconId / 1000, (hq ? "hq/" : "") + lang, iconId, highres ? "_hr1" : "");
-
-        if (PenumbraIPC.Instance is {Enabled: true} && !forceOriginal && XIVDeckPlugin.Instance.Configuration.UsePenumbraIPC)
-            path = PenumbraIPC.Instance.ResolvePenumbraPath(path);
-
-        return path;
+    private static string GetIconPath(string lang, int iconId, bool hq = false, bool highres = false) {
+        return string.Format(IconFileFormat, iconId / 1000, (hq ? "hq/" : "") + lang, iconId, highres ? "_hr1" : "");
     }
 
-    public static TexFile? GetIcon(string lang, int iconId, bool hq = false, bool highres = false) {
+    public static TexFile? GetTexture(string texPath, bool forceOriginal = false) {
         TexFile? texFile;
-        
-        if (lang.Length > 0 && !lang.EndsWith("/"))
-            lang += "/";
-        
-        var texPath = GetIconPath(lang, iconId, hq, true);
+
+        if (PenumbraIPC.Instance is {Enabled: true} && XIVDeckPlugin.Instance.Configuration.UsePenumbraIPC &&
+            !forceOriginal)
+            texPath = PenumbraIPC.Instance.ResolvePenumbraPath(texPath);
 
         if (texPath.Substring(1, 2) == ":\\") {
             PluginLog.Verbose($"Using on-disk asset {texPath}");
@@ -35,6 +29,17 @@ public static class IconManager {
         } else {
             texFile = Injections.DataManager.GetFile<TexFile>(texPath);
         }
+
+        return texFile;
+    }
+
+
+    public static TexFile? GetIcon(string lang, int iconId, bool hq = false, bool highres = false) {
+        if (lang.Length > 0 && !lang.EndsWith("/"))
+            lang += "/";
+
+        var texPath = GetIconPath(lang, iconId, hq, true);
+        var texFile = GetTexture(texPath);
 
         // recursion steps:
         // - attempt to get the high-res file exactly as described.
@@ -54,7 +59,28 @@ public static class IconManager {
                 return texFile;
         }
     }
-    
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="uldName"></param>
+    /// <param name="partId"></param>
+    /// <param name="subPartId"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException">Thrown when the requested ULD does not exist.</exception>
+    /// <exception cref="IndexOutOfRangeException">Thrown when partID/subPartID is not found in the ULD.</exception>
+    public static (int X, int Y, int W, int H) GetUldRect(string uldName, int partId, int subPartId) {
+        var uldFile = Injections.DataManager.GetFile<UldFile>($"ui/uld/{uldName}.uld");
+        if (uldFile is null) {
+            throw new ArgumentException($"The ULD {uldName} does not exist.");
+        }
+        
+        var partsData = uldFile.Parts[partId];
+        var part = partsData.Parts[subPartId];
+
+        return (part.U, part.V, part.W, part.H);
+    }
+
     public static string GetIconAsPngString(int iconId, bool hq = false) {
         var icon = GetIcon("", iconId, hq, true) ?? GetIcon("", 0, hq, true)!;
 
--- FFXIVPlugin/Server/Controllers/IconController.cs
+++ FFXIVPlugin/Server/Controllers/IconController.cs
@@ -1,10 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using EmbedIO;
 using EmbedIO.Routing;
 using EmbedIO.WebApi;
+using SixLabors.ImageSharp.Processing;
 using XIVDeck.FFXIVPlugin.Game.Managers;
 using XIVDeck.FFXIVPlugin.Server.Helpers;
 using XIVDeck.FFXIVPlugin.Utils;
+using Rectangle = SixLabors.ImageSharp.Rectangle;
 
 namespace XIVDeck.FFXIVPlugin.Server.Controllers;
 
@@ -22,6 +25,44 @@ public class IconController : WebApiController {
 
         await using var stream = this.HttpContext.OpenResponseStream();
         await stream.WriteAsync(icon.GetImage().ConvertToPng());
+    }
+
+    [Route(HttpVerbs.Get, "/uld/{uldName}")]
+    public async Task GetUldTexture(string uldName, [QueryField] int? partId, [QueryField] int? subPartId, [QueryField] bool? highres) {
+        this.HttpContext.Response.ContentType = "image/png";
+        highres ??= true;
+
+        var uldPath = $"ui/uld/{uldName}";
+        var tex = IconManager.GetTexture($"{uldPath}{(highres.Value ? "_hr1" : "")}.tex");
+
+        if (tex == null)
+            throw HttpException.NotFound($"Texture for {uldPath} was not found.");
+
+        var image = tex.GetImage();
+
+        if (partId != null && subPartId != null) {
+            (int X, int Y, int W, int H) uldRect;
+            var scale = highres.Value ? 2 : 1;
+
+            try {
+                uldRect = IconManager.GetUldRect(uldName, partId.Value, subPartId.Value);
+            } catch (ArgumentException ex) {
+                throw HttpException.NotFound($"ULD {uldName} was not found", ex);
+            } catch (IndexOutOfRangeException ex) {
+                throw HttpException.NotFound($"Part {partId}.{subPartId} was not found in ULD {uldName}", ex);
+            }
+            
+            image.Mutate(t => t.Crop(Rectangle.FromLTRB(
+                    scale * (uldRect.X), 
+                    scale * (uldRect.Y), 
+                    scale * (uldRect.X + uldRect.W),
+                    scale * (uldRect.Y + uldRect.H)
+                )
+            ));
+        }
+
+        await using var stream = this.HttpContext.OpenResponseStream();
+        await stream.WriteAsync(image.ConvertToPng());
     }
 }
 
